0fc643659ae6e15ea008f64a34a2a5bf1703828a
abubakar01914877870@gmail.com <abubakar01914877870@gmail.com>
2019-05-30 19:47:30 +0600

    added process to create webdriver dinamically

---
+++ src/test/java/pages/PageSignupOne.java
@@ -0,0 +1,30 @@
+package pages;
+
+import base.BaseUtil;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class MarketingSignUppageOne {
+
+    //private BaseUtil base;
+
+    public MarketingSignUppageOne(WebDriver driver) {
+
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(how = How.NAME, using = "first_name")
+    public WebElement firstName;
+
+    @FindBy(how = How.NAME, using = "last_name")
+    public WebElement lastName;
+
+    public void setSignUpPageOneInformation(String first_Name, String last_Name)
+    {
+        firstName.sendKeys(first_Name);
+        lastName.sendKeys(last_Name);
+    }
+}
---
+++ src/test/java/steps/MarketingSignUpSteps.java
@@ -0,0 +1,9 @@
+package steps;
+
+import cucumber.api.java.en.When;
+
+public class MarketingSignUp {
+    @When("I input information page one")
+    public void iInputInformationPageOne() {
+    }
+}
